c265b05278c070eb8101dd21e58d58ee0a08dc7f
Roshan Pathak <pathak.ros12345@gmail.com>
2020-04-03 09:12:19 +0530

    Used google map

--- src/main/java/com/coronavirus/coronavirustracker/service/CoronaVirusService.java
+++ src/main/java/com/coronavirus/coronavirustracker/service/CoronaVirusService.java
@@ -19,6 +19,7 @@ import com.coronavirus.coronavirustracker.model.Corona;
 import com.coronavirus.coronavirustracker.model.CoronaIndia;
 import com.coronavirus.coronavirustracker.model.Deaths;
 import com.coronavirus.coronavirustracker.model.Example;
+import com.coronavirus.coronavirustracker.model.KeyValue;
 import com.coronavirus.coronavirustracker.model.Recovered;
 import com.coronavirus.coronavirustracker.model.Statewise;
 import com.coronavirus.coronavirustracker.model.SuspectedCases;
@@ -79,15 +80,15 @@ public class CoronaVirusService {
         try {
         	ResponseEntity<Example> responseStateWise = restTemplate.exchange(INDIA_CASES_URL, HttpMethod.GET, request, new ParameterizedTypeReference<Example>() {
 			});
-        	CasesTimeSeries cts = new CasesTimeSeries();
+        	KeyValue cts = new KeyValue();
         	Example exampleResponse = new  Example();
     		exampleResponse = responseStateWise.getBody();
-    		cts = exampleResponse.getCasesTimeSeries().get(exampleResponse.getCasesTimeSeries().size()-1);
+    		cts = exampleResponse.getKeyValues().get(0);
     		CoronaIndia coronaIndia = new CoronaIndia();
-    		coronaIndia.setPreviousConfirmed(cts.getDailyconfirmed());
-    		coronaIndia.setPreviousDay(cts.getDate());
-    		coronaIndia.setPreviousDeaths(cts.getDailydeceased());
-    		coronaIndia.setPreviousRecovered(cts.getDailyrecovered());
+    		coronaIndia.setPreviousConfirmed(cts.getConfirmeddelta());
+    		coronaIndia.setPreviousDay(cts.getLastupdatedtime().substring(0, 10));
+    		coronaIndia.setPreviousDeaths(cts.getDeceaseddelta());
+    		coronaIndia.setPreviousRecovered(cts.getRecovereddelta());
     		return coronaIndia;
         }catch(Exception e) {
         	CoronaIndia coronaIndia = new CoronaIndia();
